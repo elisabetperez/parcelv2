acce4acc34a91515812baf9c28e40df892545535
Eli <eli@landt.co>
2023-02-01 10:52:36 +0100

    Updates

--- piechart.0cab1a34.js
+++
@@ -1,2 +0,0 @@
-var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},o={},n={},r=e.parcelRequire8311;null==r&&((r=function(e){if(e in o)return o[e].exports;if(e in n){var r=n[e];delete n[e];var l={id:e,exports:{}};return o[e]=l,r.call(l.exports,l,l.exports),l.exports}var i=new Error("Cannot find module '"+e+"'");throw i.code="MODULE_NOT_FOUND",i}).register=function(e,o){n[e]=o},e.parcelRequire8311=r);var l=r("2YFJl");console.log(l);
-//# sourceMappingURL=piechart.0cab1a34.js.map

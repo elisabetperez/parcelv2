8bd7b57a65f0d4e7ec92af58cb2be47647132137
Eli <eli@landt.co>
2023-02-01 10:16:41 +0100

    console log d3

--- charts/BarChart.js
+++ charts/BarChart.js
@@ -1,92 +1,94 @@
 import "../index.scss";
 
 import * as d3 from "d3";
-import data from "../data.json";
+console.log(d3);
 
-const select = document.querySelector("#years");
+// import data from "../data.json";
 
-let datos;
+// const select = document.querySelector("#years");
 
-select.addEventListener("change", () => {
-    if (select.value == "2020") {
-        datos = data.twenty;
-    }
-    if (select.value == "2021") {
-        datos = data.twentyOne;
-    }
+// let datos;
 
-    barChart(datos);
-});
+// select.addEventListener("change", () => {
+//     if (select.value == "2020") {
+//         datos = data.twenty;
+//     }
+//     if (select.value == "2021") {
+//         datos = data.twentyOne;
+//     }
 
-function barChart(data) {
-    const names = data.map((a) => a.name);
-    const percents = data.map((a) => a.percent);
-    const colors = data.map((a) => a.color);
+//     barChart(datos);
+// });
 
-    const svg = d3.select("#bar-graph");
-    const margin = 200;
-    const width = svg.attr("width") - margin;
-    const height = svg.attr("height") - margin;
+// function barChart(data) {
+//     const names = data.map((a) => a.name);
+//     const percents = data.map((a) => a.percent);
+//     const colors = data.map((a) => a.color);
 
-    const xScale = d3.scaleBand().range([0, width]).padding(0.7);
-    const yScale = d3.scaleLinear().range([height, 0]);
+//     const svg = d3.select("#bar-graph");
+//     const margin = 200;
+//     const width = svg.attr("width") - margin;
+//     const height = svg.attr("height") - margin;
 
-    const g = svg.append("g").attr("transform", "translate(" + 200 + "," + 100 + ")");
+//     const xScale = d3.scaleBand().range([0, width]).padding(0.7);
+//     const yScale = d3.scaleLinear().range([height, 0]);
 
-    xScale.domain(
-        data.map(function (d) {
-            return d.name;
-        })
-    );
-    yScale.domain([
-        0,
-        d3.max(data, function (d) {
-            return d.percent;
-        }),
-    ]);
+//     const g = svg.append("g").attr("transform", "translate(" + 200 + "," + 100 + ")");
 
-    // Eje X
-    g.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(xScale))
-        .append("text")
-        .attr("y", height - 250)
-        .attr("x", width - 100)
-        .attr("text-anchor", "end")
-        .attr("stroke", "black");
+//     xScale.domain(
+//         data.map(function (d) {
+//             return d.name;
+//         })
+//     );
+//     yScale.domain([
+//         0,
+//         d3.max(data, function (d) {
+//             return d.percent;
+//         }),
+//     ]);
 
-    // Eje Y
-    g.append("g")
-        .call(
-            d3
-                .axisLeft(yScale)
-                .tickFormat(function (d) {
-                    return d;
-                })
-                .ticks(5)
-        )
-        .append("text")
-        .attr("y", height / 2)
-        .attr("x", -50)
-        .attr("text-anchor", "end")
-        .attr("stroke", "black")
-        .text("Defects / 1000 cars");
+//     // Eje X
+//     g.append("g")
+//         .attr("transform", "translate(0," + height + ")")
+//         .call(d3.axisBottom(xScale))
+//         .append("text")
+//         .attr("y", height - 250)
+//         .attr("x", width - 100)
+//         .attr("text-anchor", "end")
+//         .attr("stroke", "black");
 
-    g.selectAll(".bar")
-        .data(data)
-        .enter()
-        .append("rect")
-        .attr("class", "bar")
-        .attr("x", function (d) {
-            return xScale(d.name);
-        })
-        .attr("fill", "#FF0000")
+//     // Eje Y
+//     g.append("g")
+//         .call(
+//             d3
+//                 .axisLeft(yScale)
+//                 .tickFormat(function (d) {
+//                     return d;
+//                 })
+//                 .ticks(5)
+//         )
+//         .append("text")
+//         .attr("y", height / 2)
+//         .attr("x", -50)
+//         .attr("text-anchor", "end")
+//         .attr("stroke", "black")
+//         .text("Defects / 1000 cars");
 
-        .attr("y", function (d) {
-            return yScale(d.percent);
-        })
-        .attr("width", xScale.bandwidth())
-        .attr("height", function (d) {
-            return height - yScale(d.percent);
-        });
-}
+//     g.selectAll(".bar")
+//         .data(data)
+//         .enter()
+//         .append("rect")
+//         .attr("class", "bar")
+//         .attr("x", function (d) {
+//             return xScale(d.name);
+//         })
+//         .attr("fill", "#FF0000")
+
+//         .attr("y", function (d) {
+//             return yScale(d.percent);
+//         })
+//         .attr("width", xScale.bandwidth())
+//         .attr("height", function (d) {
+//             return height - yScale(d.percent);
+//         });
+// }
